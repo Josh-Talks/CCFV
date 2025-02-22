56a0e7afbd5d7e3242bb3c4a14f87ed4ff04a9a4
Joshua Talks <joshua.talks@embl.de>
2025-02-22 20:25:21 +0100

    ensure no numerical errors

--- utils/ccfv.py
+++ utils/ccfv.py
@@ -6,9 +6,20 @@ def cal_w_distance(f1, f2):
     miu_f2 = np.mean(f2, axis=0)
     cov_f1 = np.cov(f1.T)
     cov_f2 = np.cov(f2.T)
+
+    # Add a small regularization term to the diagonals of the covariance matrices
+    cov_f1 += np.eye(cov_f1.shape[0]) * 1e-6
+    cov_f2 += np.eye(cov_f2.shape[0]) * 1e-6
+
+    sqrt_cov_product = sqrtm(cov_f1.dot(cov_f2))
+
+    # Ensure the result is real
+    if np.iscomplexobj(sqrt_cov_product):
+        sqrt_cov_product = np.real(sqrt_cov_product)
+
     delta_miu = miu_f1 - miu_f2
     w_d = np.sum(delta_miu**2) + cov_f1.trace() + cov_f2.trace() - \
-        2 * (sqrtm((cov_f1.dot(cov_f2)))).trace()
+        2 * sqrt_cov_product.trace()
 
     return np.sqrt(abs(w_d))
 
